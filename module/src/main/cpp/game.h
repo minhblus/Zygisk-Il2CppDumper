f269556077f7efced557547ddada51f9e5b454b4
minhblus <phamvanminh140600@gmail.com>
2026-03-15 20:52:09 +0700

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -5,6 +5,6 @@
 #ifndef ZYGISK_IL2CPPDUMPER_GAME_H
 #define ZYGISK_IL2CPPDUMPER_GAME_H
 
-#define GamePackageName "GamePackageName"
+#define GamePackageName "com.game.packagename"
 
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
